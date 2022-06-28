d73b42d329d5482ad77116ea8c7d13f16d870680
RomanRabinovich <roman.kliakhin@gmail.com>
2022-06-28 22:07:26 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: TextField(controller: controller),
+          actions: [
+            IconButton(
+              onPressed: () {
+                final name = controller.text;
+
+                createUser(name: name);
+              },
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      );
+
+  Future createUser({required String name}) async {
+    //Reference to document
+    final docUser = FirebaseFirestore.instance.collection('users').doc('my-id');
+
+    final json = {
+      'name': name,
+    };
+
+    /// Create document and write data to Firebase
+    await docUser.set(json);
+  }
+}
